86ed90612e3ad81e59c71c6e51d8f10b7613252c
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-03-22 13:30:58 -0700

    basic outline done

--- mass-mail.py
+++ mass-mail.py
@@ -1,4 +1,16 @@
 import argparse
+import pandas
+from os import path
+
+data = None
+
+# parse data.csv
+def parse_data():
+    if not path.isfile("data.csv"):
+        raise Exception("data.csv does not exist")
+    df = pandas.read_csv("data.csv")
+    print(df)
+    print(len(df))
 
 # generates a sample email from data
 def generate_sample():
@@ -9,13 +21,16 @@ def getOptions():
     aparser = argparse.ArgumentParser(description="email parameters")
     aparser.add_argument('-s', '--sample', required=False, type=bool, help='view sample emails')
     aparser.add_argument('-t', '--test', required=False, type=bool, help='test email')
+    aparser.add_argument("-sample", action="store_true", help="view sample emails")
+    aparser.add_argument("-test", action="store_true", help="test email")
     opts = vars( aparser.parse_args() )
     return opts
+
 
 def main():
     opts = getOptions()
     if opts["sample"]:
-        # s = generate_sample()
+        s = generate_sample()
         print("sample")
     if opts["test"]:
         # generate_test()
@@ -23,4 +38,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main() 
+    main()

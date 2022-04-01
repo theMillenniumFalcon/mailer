d2b0335e5b7f5439d7503b6443d784fa3acca299
theMillenniumFalcon <nishankpr2002@gmail.com>
2022-04-01 18:10:54 +0530

    removed dead code

--- mailer.py
+++ mailer.py
@@ -1,15 +1,27 @@
 import argparse
-from re import I
+import pandas
+from os import path
+
+data = None
+
+# parse data.csv
+def parse():
+    if not path.isfile("data.csv"):
+        raise Exception("cannot find data.csv file")
+    result = pandas.read_csv("data.csv")
+    print(result)
+    print(len(result))
 
 # generates a sample mail from the data
 def generate():
+    parse()
     return
 
 # function for getting command line arguments
 def getOptions():
     parser = argparse.ArgumentParser(description = "email parameters")
-    parser.add_argument('-s', '--sample', required = False, type = bool, help = 'view sample emails')
-    parser.add_argument('-t', '--test', required = False, type = bool, help = 'test email')
+    parser.add_argument('-sample', action="store_true", help = "view sample emails")
+    parser.add_argument('-test', action="store_true", help = "test emai")
 
     opts = vars(parser.parse_args())
 
@@ -19,6 +31,7 @@ def main():
     opts = getOptions()
 
     if opts["sample"]:
+        s = generate()
         print("sample")
     
     if opts["test"]:
